e11aa95ae5c9e51bc2cd07771198dae71e0d3fbb
ttakuro <pochomukinsan@gmail.com>
2022-04-01 09:47:50 -0700

    put the tx button after txinfo

--- src/page/Home/component/TradeV2/Summary.tsx
+++ src/page/Home/component/TradeV2/Summary.tsx
@@ -8,15 +8,15 @@ function Summary() {
             <Heading w="full" size="md">
                 Transaction Summary
             </Heading>
+            <Box width="100%" borderStyle="solid" borderWidth="1px" borderColor="gray.200" borderRadius="12px" mb="4">
+                <TxInfoTable />
+            </Box>
             <VStack spacing={2} width="full">
                 <SendTxButton />
                 <Text fontSize="sm" color="gray.500">
                     Confirm in Metamask
                 </Text>
             </VStack>
-            <Box width="100%" borderStyle="solid" borderWidth="1px" borderColor="gray.200" borderRadius="12px">
-                <TxInfoTable />
-            </Box>
         </>
     )
 }
