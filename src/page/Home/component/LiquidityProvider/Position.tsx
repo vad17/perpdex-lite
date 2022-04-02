baebbae4a34f004c6e559082616edbc3cc3046e0
ttakuro <pochomukinsan@gmail.com>
2022-04-02 11:25:41 -0700

    implemented Liquidity positions template design

--- src/page/Home/component/LiquidityProvider/Position.tsx
+++ src/page/Home/component/LiquidityProvider/Position.tsx
@@ -4,14 +4,13 @@ import { Box, Button, Heading, HStack } from "@chakra-ui/react"
 function Position() {
     return (
         <>
-            <HStack>
-                <Heading size="sm">Liquidity Positions</Heading>
-                <Button leftIcon={<AddIcon />} colorScheme="pink" variant="solid" size="xs">
-                    Add Liquidity
-                </Button>
-            </HStack>
-            <Box width="100%" borderStyle="solid" borderWidth="1px" borderColor="gray.200" borderRadius="12px">
-                aa
+            <Box width="100%">
+                <HStack justifyContent="space-between">
+                    <Heading size="sm">Liquidity Positions</Heading>
+                    <Button leftIcon={<AddIcon />} colorScheme="pink" variant="solid" size="xs">
+                        Add Liquidity
+                    </Button>
+                </HStack>
             </Box>
         </>
     )
