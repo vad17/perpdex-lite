dc42c8e9d1438f5a545797f0ba18f3341aa642cc
a <a@example.com>
2022-04-09 23:46:44 +0900

    fix liquidity provider title bar

--- src/page/Home/component/LiquidityProvider/TitleBar.tsx
+++ src/page/Home/component/LiquidityProvider/TitleBar.tsx
@@ -1,17 +1,20 @@
 import { HStack, Text, VStack } from "@chakra-ui/react"
 
 import React from "react"
+import { Amm } from "../../../../container/amm"
 
 function TitleBar() {
+    const { selectedAmm } = Amm.useContainer()
+
     return (
         <HStack justifyContent="start">
             <VStack spacing={-2} alignItems="start">
                 <Text fontSize="lg" fontWeight="bold">
-                    ASTR
+                    {selectedAmm?.baseAssetSymbol}/{selectedAmm?.quoteAssetSymbol}
                 </Text>
-                <Text fontSize="xs" color="gray.500">
-                    Astar
-                </Text>
+                {/*<Text fontSize="xs" color="gray.500">*/}
+                {/*    ETH*/}
+                {/*</Text>*/}
             </VStack>
         </HStack>
     )
