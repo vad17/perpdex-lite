3975e983a2974973367c708f7a7ae96d3dbbcc66
a <a@example.com>
2022-04-09 23:44:09 +0900

    display maker position info

--- src/page/Home/component/LiquidityProvider/ProvidedInfo.tsx
+++ src/page/Home/component/LiquidityProvider/ProvidedInfo.tsx
@@ -14,8 +14,73 @@ import {
     Flex,
     HStack,
 } from "@chakra-ui/react"
+import { useCallback, useEffect, useState } from "react"
+import { Contract as MulticallContract } from "ethers-multicall/dist/contract"
+import { ContractCall } from "ethers-multicall"
+import { PnlCalcOption } from "../../../../constant"
+import { bigNum2Big, decimal2Big } from "../../../../util/format"
+import { useInterval } from "@chakra-ui/hooks"
+import { Connection } from "../../../../container/connection"
+import { Amm } from "../../../../container/amm"
+import { NewContract } from "../../../../container/newContract"
+import Big from "big.js"
+import { useRealtimeAmm } from "../../../../hook/useRealtimeAmm"
+
+export interface MakerPositionInfo {
+    unrealizedPnl: Big
+    liquidityValue: Big
+}
 
 function ProvidedInfoTable() {
+    const { account } = Connection.useContainer()
+    const { addressMap, orderBook } = NewContract.useContainer()
+    const { ammMap, selectedAmm } = Amm.useContainer()
+    const baseTokenAddress = selectedAmm?.address || ""
+    const ammName = selectedAmm?.baseAssetSymbol || ""
+    const { price } = useRealtimeAmm(baseTokenAddress, ammName)
+    const [makerPositionInfo, setMakerPositionInfo] = useState<MakerPositionInfo>({
+        unrealizedPnl: Big(0),
+        liquidityValue: Big(0),
+    })
+
+    const getMakerPositionInfo = useCallback(async () => {
+        if (!account) return
+        if (!orderBook) return
+        if (!baseTokenAddress) return
+        if (!price) return
+
+        const [quoteAmountRaw, quotePendingFee] = await orderBook.getTotalTokenAmountInPoolAndPendingFee(
+            account,
+            baseTokenAddress,
+            false,
+        )
+        const [baseAmountRaw, basePendingFee] = await orderBook.getTotalTokenAmountInPoolAndPendingFee(
+            account,
+            baseTokenAddress,
+            true,
+        )
+        const quoteDebtRaw = await orderBook.getTotalOrderDebt(account, baseTokenAddress, false)
+        const baseDebtRaw = await orderBook.getTotalOrderDebt(account, baseTokenAddress, true)
+
+        const baseAmount = bigNum2Big(baseAmountRaw)
+        const quoteAmount = bigNum2Big(quoteAmountRaw)
+        const baseDebt = bigNum2Big(baseDebtRaw)
+        const quoteDebt = bigNum2Big(quoteDebtRaw)
+
+        const info = {
+            liquidityValue: baseAmount.mul(price).add(quoteAmount),
+            unrealizedPnl: baseAmount.sub(baseDebt).mul(price).add(quoteAmount.sub(quoteDebt)),
+        }
+
+        setMakerPositionInfo(info)
+    }, [account, orderBook, baseTokenAddress, price, setMakerPositionInfo])
+
+    useEffect(() => {
+        getMakerPositionInfo()
+    }, [getMakerPositionInfo])
+
+    useInterval(getMakerPositionInfo, 5000)
+
     return (
         <>
             <Heading w="full" size="sm">
@@ -27,12 +92,12 @@ function ProvidedInfoTable() {
                         Total Liquidity
                     </Text>
                     <Text fontSize="xl" fontWeight="bold" lineHeight="1.4">
-                        $-,--
+                        $ {makerPositionInfo.liquidityValue.toFixed(6)}
                     </Text>
                 </Box>
                 <Box>
                     <Text fontSize="xs" color="gray.500">
-                        Total Fees{" "}
+                        Unrealized Pnl{" "}
                         <Popover trigger="hover">
                             <PopoverTrigger>
                                 <QuestionOutlineIcon></QuestionOutlineIcon>
@@ -45,7 +110,7 @@ function ProvidedInfoTable() {
                         </Popover>
                     </Text>
                     <Text fontSize="xl" color="green.400" fontWeight="bold" lineHeight="1.4">
-                        $-,--
+                        $ {makerPositionInfo.unrealizedPnl.toFixed(6)}
                     </Text>
                 </Box>
             </SimpleGrid>
