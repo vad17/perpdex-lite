6883efbe26936a3e60ff02e39317a3c709455928
a <a@example.com>
2022-04-12 14:53:27 +0900

    Replaced xDai message with rinkeby

--- src/page/Home/component/GetStarted/index.tsx
+++ src/page/Home/component/GetStarted/index.tsx
@@ -13,8 +13,8 @@ function GetStarted() {
                     <ExternalLink color="blue.500" href="https://perpdex.com" isExternal>
                         PerpDEX Protocol <ExternalLinkIcon mx="2px" />
                     </ExternalLink>{" "}
-                    UI with essential features. Note that you need to manually switch to <strong>xDai network</strong>{" "}
-                    to trade.
+                    UI with essential features. Note that you need to manually switch to{" "}
+                    <strong>rinkeby network</strong> to trade.
                 </Text>
                 <HStack>
                     <ExternalLink color="blue.500" href="https://github.com/perpdex/perpdex-lite" isExternal>
@@ -25,19 +25,19 @@ function GetStarted() {
                     </ExternalLink>
                 </HStack>
             </Stack>
-            <Divider />
-            <Stack spacing={5}>
-                <Heading size="md">Switch Network</Heading>
-                <Text>
-                    You're currently on <strong>Ethereum Mainnet</strong>{" "}
-                </Text>
-                <Button isFullWidth isDisabled colorScheme="blue">
-                    Add / Switch to xDai Network
-                </Button>
-                <Text fontSize="sm" color="gray.500">
-                    This feature is not available for now.
-                </Text>
-            </Stack>
+            {/*<Divider />*/}
+            {/*<Stack spacing={5}>*/}
+            {/*    <Heading size="md">Switch Network</Heading>*/}
+            {/*    <Text>*/}
+            {/*        You're currently on <strong>Ethereum Mainnet</strong>{" "}*/}
+            {/*    </Text>*/}
+            {/*    <Button isFullWidth isDisabled colorScheme="blue">*/}
+            {/*        Add / Switch to xDai Network*/}
+            {/*    </Button>*/}
+            {/*    <Text fontSize="sm" color="gray.500">*/}
+            {/*        This feature is not available for now.*/}
+            {/*    </Text>*/}
+            {/*</Stack>*/}
         </Stack>
     )
 }
