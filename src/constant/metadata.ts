5f3894278e5b4cb2fe526901daabc298b5c6e829
Kei Shimizu <suns.manage.k@gmail.com>
2022-05-28 21:06:12 +0900

    added settlementToken for deposit token

--- src/constant/metadata.ts
+++ src/constant/metadata.ts
@@ -8,6 +8,7 @@ export const metadata = {
                         chainId: 4,
                         contracts: {
                             clearingHouse: "0x155BdFf407997A090Acd8D3d56B5ACB492f34E7a",
+                            settlementToken: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
                             quoteToken: "0xCDC9AefE9E78A76e666B48daED81A2022c3e2B86",
                             baseTokens: {
                                 usd: "0x9c3E3ecfEdBafEA2CF85EBA0B3a6d8cA408Ab22E",
