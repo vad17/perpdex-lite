aacd9717516cfa68bec43c0801f3b9ffae1c320b
Kei Shimizu <suns.manage.k@gmail.com>
2022-06-12 13:37:34 +0900

    removed reducer from perpdexExchangeContainer

--- src/container/perpdexExchangeContainer/index.tsx
+++ src/container/perpdexExchangeContainer/index.tsx
@@ -1,6 +1,6 @@
 import { BIG_NUMBER_ZERO, Network } from "../../constant"
 import { big2BigNum, parseEther } from "util/format"
-import { useCallback, useEffect, useReducer } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 import { Big } from "big.js"
 import { PerpdexExchangeActions } from "./type"
@@ -12,49 +12,28 @@ import { createContainer } from "unstated-next"
 import { PerpdexMarketContainer } from "container/perpdexMarketContainer"
 import { BigNumber } from "ethers"
 
-enum ACTIONS {
-    UPDATE_EXECUTER = "UPDATE_EXECUTER",
-}
-
-type ActionType = { type: ACTIONS.UPDATE_EXECUTER; payload: { contractExecuter?: ContractExecutor } }
-
-const initialState = {
-    contractExecuter: undefined as ContractExecutor | undefined,
-}
-
-function reducer(state: typeof initialState, action: ActionType) {
-    switch (action.type) {
-        case ACTIONS.UPDATE_EXECUTER: {
-            return {
-                ...state,
-                contractExecuter: action.payload.contractExecuter,
-            }
-        }
-        default:
-            throw new Error()
-    }
-}
-
 export const PerpdexExchangeContainer = createContainer(usePerpdexExchangeContainer)
 
 export interface Executors {
-    [Network.Mumbai]: PerpdexExchangeActions
-    // [Network.Xdai]: ClearingHousePerpdexActions,
+    [Network.Mumbai]: PerpdexExchangeActions // FIX: support chains
 }
 
 function usePerpdexExchangeContainer() {
-    const [state, dispatch] = useReducer(reducer, initialState)
     const { account, signer } = Connection.useContainer()
     const { perpdexExchange } = Contract.useContainer()
     const perpdexMarketState = PerpdexMarketContainer.useContainer()
     const { execute } = Transaction.useContainer()
 
+    /**
+     * state of perpdexExchangeContiner
+     */
+    const [contractExecuter, setContractExecuter] = useState<ContractExecutor | undefined>(undefined)
+
     useEffect(() => {
-        if (!perpdexExchange) return
-
-        const contractExecuter = new ContractExecutor(perpdexExchange, signer)
-
-        dispatch({ type: ACTIONS.UPDATE_EXECUTER, payload: { contractExecuter } })
+        if (perpdexExchange) {
+            const _contractExecuter = new ContractExecutor(perpdexExchange, signer)
+            setContractExecuter(_contractExecuter)
+        }
     }, [perpdexExchange, signer])
 
     useEffect(() => {
@@ -76,36 +55,36 @@ function usePerpdexExchangeContainer() {
 
     const deposit = useCallback(
         (amount: string) => {
-            if (state.contractExecuter) {
-                execute(state.contractExecuter.deposit(parseEther(amount), BIG_NUMBER_ZERO))
+            if (contractExecuter) {
+                execute(contractExecuter.deposit(parseEther(amount), BIG_NUMBER_ZERO))
             }
         },
-        [execute, state.contractExecuter],
+        [contractExecuter, execute],
     )
 
     const withdraw = useCallback(
         (amount: string) => {
-            if (state.contractExecuter) {
-                execute(state.contractExecuter.withdraw(parseEther(amount)))
+            if (contractExecuter) {
+                execute(contractExecuter.withdraw(parseEther(amount)))
             }
         },
-        [execute, state.contractExecuter],
+        [contractExecuter, execute],
     )
 
     const closePosition = useCallback(
         (baseToken: string, quoteAmountBound: Big) => {
-            if (state.contractExecuter) {
-                execute(state.contractExecuter.closePosition(baseToken, big2BigNum(quoteAmountBound)))
+            if (contractExecuter) {
+                execute(contractExecuter.closePosition(baseToken, big2BigNum(quoteAmountBound)))
             }
         },
-        [execute, state.contractExecuter],
+        [contractExecuter, execute],
     )
 
     const openPosition = useCallback(
         (isBaseToQuote: boolean, isExactInput: boolean, amount: BigNumber, oppositeAmountBount: BigNumber) => {
-            if (state.contractExecuter && account && perpdexMarketState.state.currentMarket) {
+            if (contractExecuter && account && perpdexMarketState.state.currentMarket) {
                 execute(
-                    state.contractExecuter.openPosition(
+                    contractExecuter.openPosition(
                         account,
                         perpdexMarketState.state.currentMarket.baseAddress,
                         isBaseToQuote,
@@ -118,14 +97,14 @@ function usePerpdexExchangeContainer() {
                 )
             }
         },
-        [account, execute, perpdexMarketState.state.currentMarket, state.contractExecuter],
+        [account, contractExecuter, execute, perpdexMarketState.state.currentMarket],
     )
 
     const addLiquidity = useCallback(
         (base: Big, quote: Big, minBase: Big, minQuote: Big) => {
-            if (state.contractExecuter && account && perpdexMarketState.state.currentMarket) {
+            if (contractExecuter && account && perpdexMarketState.state.currentMarket) {
                 execute(
-                    state.contractExecuter.addLiquidity(
+                    contractExecuter.addLiquidity(
                         perpdexMarketState.state.currentMarket.baseAddress,
                         big2BigNum(base),
                         big2BigNum(quote),
@@ -135,14 +114,14 @@ function usePerpdexExchangeContainer() {
                 )
             }
         },
-        [account, execute, perpdexMarketState.state.currentMarket, state.contractExecuter],
+        [account, contractExecuter, execute, perpdexMarketState.state.currentMarket],
     )
 
     const removeLiquidity = useCallback(
         (liquidity: Big, minBase: Big, minQuote: Big) => {
-            if (state.contractExecuter && account && perpdexMarketState.state.currentMarket) {
+            if (contractExecuter && account && perpdexMarketState.state.currentMarket) {
                 execute(
-                    state.contractExecuter.removeLiquidity(
+                    contractExecuter.removeLiquidity(
                         account,
                         perpdexMarketState.state.currentMarket.baseAddress,
                         big2BigNum(liquidity),
@@ -152,15 +131,10 @@ function usePerpdexExchangeContainer() {
                 )
             }
         },
-        [account, execute, perpdexMarketState.state.currentMarket, state.contractExecuter],
+        [account, contractExecuter, execute, perpdexMarketState.state.currentMarket],
     )
 
-    /**
-     * getter
-     */
-
     return {
-        state,
         deposit,
         withdraw,
         openPosition,
