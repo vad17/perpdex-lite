8671333cf86085fb3a314c51640d9978410b55dd
a <a@example.com>
2022-06-16 00:19:05 +0900

    add long token contract config

--- src/constant/contract.ts
+++ src/constant/contract.ts
@@ -1,19 +1,24 @@
-import perpdexDeployment from "../deployments/perpdexContract.json"
 import _ from "lodash"
+import perpdexDeployment from "../deployments/perpdexContract.json"
+import perpdexStablecoin from "../deployments/perpdexStablecoin.json"
 
 interface Contract {
     address: string
 }
 
-interface PerpdexMarket extends Contract {}
+interface PerpdexLongToken extends Contract {}
+
+interface PerpdexMarket extends Contract {
+    longToken: PerpdexLongToken
+}
 
 interface PerpdexExchange extends Contract {
-    perpdexMarkets: PerpdexMarket[]
+    markets: PerpdexMarket[]
 }
 
 interface ContractConfig {
     weth: Contract
-    perpdexExchanges: PerpdexExchange[]
+    exchanges: PerpdexExchange[]
 }
 
 export const contractConfigs: { [key: string]: ContractConfig } = {
@@ -21,31 +26,43 @@ export const contractConfigs: { [key: string]: ContractConfig } = {
         weth: {
             address: "0xc778417E063141139Fce010982780140Aa0cD5Ab",
         },
-        perpdexExchanges: [],
+        exchanges: [],
     },
     81: {
         weth: {
             address: "0x9Af480478974a2fda7d5aE667541639164D2858B",
         },
-        perpdexExchanges: [],
+        exchanges: [],
     },
 }
 
 _.each(contractConfigs, (value: ContractConfig, key: string) => {
     const deployment: any = perpdexDeployment
+    const stablecoinDeployment: any = perpdexStablecoin
     const contracts = deployment[key][0].contracts
-    const perpdexMarkets: PerpdexMarket[] = []
+    const stablecoinContracts = stablecoinDeployment[key][0].contracts
+    const markets: PerpdexMarket[] = []
+    const longTokens: { [key: string]: PerpdexLongToken } = {}
+
+    _.each(stablecoinContracts, (config, contractName) => {
+        if (contractName.startsWith("PerpdexLongToken")) {
+            longTokens[contractName] = {
+                address: config.address,
+            }
+        }
+    })
 
     _.each(contracts, (config, contractName) => {
         if (contractName.startsWith("PerpdexMarket")) {
-            perpdexMarkets.push({
+            markets.push({
                 address: config.address,
+                longToken: longTokens[contractName.replace("PerpdexMarket", "PerpdexLongToken")],
             })
         }
     })
 
-    value.perpdexExchanges.push({
+    value.exchanges.push({
         address: contracts.PerpdexExchange.address,
-        perpdexMarkets: perpdexMarkets,
+        markets: markets,
     })
 })
