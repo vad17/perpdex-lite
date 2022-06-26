bfd0c2dd556677838d1304673451c5af558c46f3
ttakuro <pochomukinsan@gmail.com>
2022-06-25 19:06:11 -0700

    redesigned the account panel as Figma

--- src/component/AccountPanel/index.tsx
+++ src/component/AccountPanel/index.tsx
@@ -1,5 +1,5 @@
-import { Box, SimpleGrid, Text } from "@chakra-ui/react"
-import { LogoWhite } from "../Icon"
+import { Box, Flex, HStack, SimpleGrid, Text, VStack } from "@chakra-ui/react"
+import { CircleIcon, EthIconBlack, LogoBlack, LogoWhite, PerpCoin } from "../Icon"
 import Big from "big.js"
 import { PerpdexExchangeContainer } from "../../container/connection/perpdexExchangeContainer"
 import { PerpdexMarketContainer } from "../../container/connection/perpdexMarketContainer"
@@ -15,33 +15,56 @@ function AccountPanel() {
     const collateralBalanceUsd = collateralBalance.mul(currentMarketState.indexPriceQuote)
 
     return (
-        <Box bgGradient="linear(to-b, #627EEA80, #F9007780)" borderRadius={20}>
-            <SimpleGrid width="100%" columns={2} spacing={6} padding={59}>
-                <LogoWhite></LogoWhite>
-                <Box></Box>
-                <Box>
-                    <Text fontSize="xs" color="white">
-                        Total Account Value
-                    </Text>
-                    <Text as="span" fontSize="xl" fontWeight="bold">
-                        {totalAccountValue?.toFixed()} {quoteSymbol}
-                    </Text>
-                    <Text as="span" fontSize="xl" marginLeft={2}>
-                        (${totalAccountValueUsd?.toFixed()})
-                    </Text>
-                </Box>
-                <Box>
-                    <Text fontSize="xs" color="white">
-                        Collateral
-                    </Text>
-                    <Text as="span" fontSize="xl" fontWeight="bold">
-                        {collateralBalance?.toFixed()} {quoteSymbol}
-                    </Text>
-                    <Text as="span" fontSize="xl" marginLeft={2}>
-                        (${collateralBalanceUsd?.toFixed()})
-                    </Text>
-                </Box>
-            </SimpleGrid>
+        <Box bgColor={"#D9D9D9"} borderRadius={20} padding={59}>
+            <VStack spacing={28} alignItems="stretch">
+                <HStack w="100%" justifyContent="space-between" alignItems="center">
+                    <LogoBlack></LogoBlack>
+                    <Box></Box>
+                </HStack>
+                <HStack w="100%" justifyContent="space-between" alignItems="center">
+                    <Box>
+                        <HStack>
+                            <CircleIcon boxSize={12} color="blackAlpha.300" />
+                            <VStack spacing={0}>
+                                <Text fontSize="xs" color="black">
+                                    Net USD Value
+                                </Text>
+                                <HStack w="100%" justifyContent="start">
+                                    <Text as="span" color="black" fontSize="xl" fontWeight="bold">
+                                        {totalAccountValue?.toFixed()} {quoteSymbol}
+                                    </Text>
+                                    <Text as="span" color="black" fontSize="xl" marginLeft={2}>
+                                        (${totalAccountValueUsd?.toFixed()})
+                                    </Text>
+                                </HStack>
+                            </VStack>
+                        </HStack>
+                    </Box>
+
+                    <Box>
+                        <HStack>
+                            <CircleIcon boxSize={12} color="blackAlpha.300" />
+                            <VStack spacing={0}>
+                                <Text fontSize="xs" color="black">
+                                    Free Collateral
+                                </Text>
+                                <HStack w="100%" justifyContent="start">
+                                    <Text as="span" color="black" fontSize="xl" fontWeight="bold">
+                                        {collateralBalance?.toFixed()} {quoteSymbol}
+                                    </Text>
+                                    <Text as="span" color="black" fontSize="xl" marginLeft={2}>
+                                        (${collateralBalanceUsd?.toFixed()})
+                                    </Text>
+                                </HStack>
+                            </VStack>
+                        </HStack>
+                    </Box>
+                </HStack>
+                <HStack w="100%" justifyContent="space-between" alignItems="center">
+                    <Box></Box>
+                    <EthIconBlack></EthIconBlack>
+                </HStack>
+            </VStack>
         </Box>
     )
 }
