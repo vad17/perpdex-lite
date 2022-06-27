3d67a1c7bd2d8bce1f63cb9b506ebb14551c3415
Kei Shimizu <suns.manage.k@gmail.com>
2022-06-28 07:40:12 +0900

    fixed hash router bugs of react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import WalletListModal from "component/WalletModal"
-import { Route, Switch } from "react-router-dom"
+import { Route, Switch, Router } from "react-router-dom"
+import { createBrowserHistory } from "history"
 import Header from "./component/Header"
 import Home from "./page/Home"
 import Trade from "./page/Trade"
@@ -13,22 +14,28 @@ import LiquidityProviderModal from "component/LiquidityProviderModal"
 import LiquidityProvider from "page/LiquidityProvider"
 import Pools from "page/Pools"
 
-export const App = () => (
-    <Container maxW="container.2xl" pb={20} px={6}>
-        <Header />
-        <Divider />
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/trade" component={Trade} />
-            <Route exact path="/pools" component={Pools} />
-            <Route exact path="/pools/:marketAddress" component={LiquidityProvider} />
-        </Switch>
-        <LiquidityProviderModal />
-        <WalletListModal />
-        <ClosePositionModal />
-        <UserAgreementModal />
-        {/* NOTE: BlockedRegionModal should be in the last one */}
-        <BlockedRegionModal />
-        <AccountPerpdexModal />
-    </Container>
-)
+export const App = () => {
+    const history = createBrowserHistory()
+
+    return (
+        <Container maxW="container.2xl" pb={20} px={6}>
+            <Router history={history}>
+                <Header />
+                <Divider />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/trade" component={Trade} />
+                    <Route exact path="/pools" component={Pools} />
+                    <Route exact path="/pools/:marketAddress" component={LiquidityProvider} />
+                </Switch>
+                <LiquidityProviderModal />
+                <WalletListModal />
+                <ClosePositionModal />
+                <UserAgreementModal />
+                {/* NOTE: BlockedRegionModal should be in the last one */}
+                <BlockedRegionModal />
+                <AccountPerpdexModal />
+            </Router>
+        </Container>
+    )
+}
