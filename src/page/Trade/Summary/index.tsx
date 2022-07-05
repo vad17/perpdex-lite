5590c059389e083b9513dd6e26f114a5daa68d4d
ttakuro <pochomukinsan@gmail.com>
2022-07-04 19:27:36 -0700

    implemented Summary design

--- src/page/Trade/Summary/index.tsx
+++ src/page/Trade/Summary/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Text, VStack } from "@chakra-ui/react"
+import { Heading } from "@chakra-ui/react"
 import TxInfoTable from "./TxInfoTable"
 import { PerpdexMarketContainer } from "container/connection/perpdexMarketContainer"
 import Big from "big.js"
@@ -10,17 +10,10 @@ function Summary() {
 
     return (
         <>
-            <Heading w="full" size="md">
+            <Heading w="full" size="md" mb={8}>
                 Transaction Summary
             </Heading>
-            <Box width="100%" borderStyle="solid" borderWidth="1px" borderColor="gray.200" borderRadius="12px">
-                <TxInfoTable markPrice={markPrice} priceImpact="" estimateGasFee={Big(0)} fundingRatio="" />
-            </Box>
-            <VStack spacing={2} width="full">
-                <Text fontSize="sm" color="gray.500">
-                    Confirm in Metamask
-                </Text>
-            </VStack>
+            <TxInfoTable markPrice={markPrice} priceImpact="" estimateGasFee={Big(0)} fundingRatio="" />
         </>
     )
 }
