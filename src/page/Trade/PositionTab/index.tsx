690e8b9dcdfa0148bdbd0ff09fdfb583d7bed863
ttakuro <pochomukinsan@gmail.com>
2022-07-04 19:28:36 -0700

    anyway commit

--- src/page/Trade/PositionTab/index.tsx
+++ src/page/Trade/PositionTab/index.tsx
@@ -1,4 +1,4 @@
-import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/react"
+import { Tabs, TabList, Tab, TabPanels, TabPanel, chakra, Table, Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react"
 import { PerpdexExchangeContainer } from "container/connection/perpdexExchangeContainer"
 import { Modal } from "container/modal"
 import { useMemo } from "react"
@@ -28,11 +28,32 @@ function PositionTab() {
         return undefined
     }, [currentMyTakerPositions, currentMarketState])
 
+    const StyledTab = chakra(Tab, {
+        baseStyle: {
+            color: "gray.200",
+            _selected: { color: "white", borderBottom: "1px solid #627EEA", paddingBottom: "2px" },
+        },
+    })
+
+    const StyledTh = chakra(Th, {
+        baseStyle: {
+            color: "white",
+            borderBottom: "0px none",
+            fontWeight: "bold",
+        },
+    })
+
+    const StyledTd = chakra(Td, {
+        baseStyle: {
+            borderBottom: "0px none",
+        },
+    })
+
     return (
-        <Tabs>
+        <Tabs variant="unstyled">
             <TabList>
-                <Tab>Positions</Tab>
-                <Tab>Closed PL</Tab>
+                <StyledTab>Positions</StyledTab>
+                <StyledTab>Order History</StyledTab>
             </TabList>
 
             <TabPanels>
@@ -52,7 +73,60 @@ function PositionTab() {
                     )}
                 </TabPanel>
                 <TabPanel>
-                    <p>Closed P&L!</p>
+                    <Table variant="simple">
+                        <Thead>
+                            <Tr>
+                                <StyledTh>Time</StyledTh>
+                                <StyledTh>Asset</StyledTh>
+                                <StyledTh>Position Size</StyledTh>
+                                <StyledTh>Market Side</StyledTh>
+                                <StyledTh>Realized PnL</StyledTh>
+                                <StyledTh>Trading Fee</StyledTh>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <StyledTd>aaaaaa</StyledTd>
+                                <StyledTd>aaaaa</StyledTd>
+                                <StyledTd>aaaaa</StyledTd>
+                                <StyledTd>aaaaa</StyledTd>
+                                <StyledTd>aaaa</StyledTd>
+                                <StyledTd>aaaaa</StyledTd>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                    {/* <Table variant="simple">
+                        <Thead>
+                            <Tr>
+                                <Th>Market</Th>
+                                <Th>Qty</Th>
+                                <Th>Value </Th>
+                                <Th>Entry Price</Th>
+                                <Th>Mark Price</Th>
+                                <Th>Liq. price</Th>
+                                <Th>Unrealzied PNL</Th>
+                                <Th>Close Position</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td>aaaaaa</Td>
+                                <Td>
+                                    <Text color={"green.300"}>aaaaaa</Text>
+                                </Td>
+                                <Td>aaaaa</Td>
+                                <Td>aaaaa</Td>
+                                <Td>aaaaa</Td>
+                                <Td>aaaa</Td>
+                                <Td>aaaaa</Td>
+                                <Td>
+                                    <Button mb={[4, 0]} colorScheme="blue">
+                                        Close Market
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table> */}
                 </TabPanel>
             </TabPanels>
         </Tabs>
--- src/page/Trade/index.tsx
+++ src/page/Trade/index.tsx
@@ -1,27 +1,116 @@
-import React from "react"
-import { VStack, Flex, Box } from "@chakra-ui/react"
+import React, { useState } from "react"
+import {
+    VStack,
+    Flex,
+    Box,
+    HStack,
+    Center,
+    Divider,
+    Text,
+    Thead,
+    Table,
+    Tr,
+    Th,
+    Tbody,
+    Td,
+    Button,
+} from "@chakra-ui/react"
 
-// import Leverage from "./Leverage"
 import MarketSelector from "component/Perpetual/MarketSelector"
 import Summary from "./Summary"
 import FrameContainer from "component/FrameContainer"
 import TradeInput from "./TradeInput"
 import PositionTab from "./PositionTab"
+import ChartHeader from "component/Perpetual/ChartHeader"
+import Slippage from "./Slippage"
 
 function Trade() {
+    const [slippage, setSlippage] = useState<number>(0.5)
+
     return (
         <FrameContainer>
             <Flex>
                 <Box flex="1" mr="2">
-                    <MarketSelector />
-                    Chart
+                    <HStack alignItems="flex-start">
+                        <VStack>
+                            <HStack justifyContent="start">
+                                <Box h="100%">
+                                    <HStack>
+                                        <MarketSelector />
+                                        <Center h="100px" mx={10}>
+                                            <Divider orientation="vertical" border="1px" borderColor="#D9D9D9" />
+                                        </Center>
+                                    </HStack>
+                                </Box>
+                                <Box w="100%" alignSelf="center">
+                                    <ChartHeader />
+                                </Box>
+                            </HStack>
+                            <Box w="100px">Chart</Box>
+                        </VStack>
+                        <Box
+                            borderColor="#728BEC"
+                            borderWidth="1px"
+                            borderRadius="10px"
+                            p={6}
+                            mx={{ base: "auto", md: "0" }}
+                        >
+                            <Text align="center" color={"gray.200"}>
+                                Order History
+                            </Text>
+                            <Table variant="simple" overflowY="scroll">
+                                <Thead>
+                                    <Tr>
+                                        <Th border="0px" color="white">
+                                            Size(ETH)
+                                        </Th>
+                                        <Th border="0px" color="white">
+                                            Price(USD)
+                                        </Th>
+                                        <Th border="0px" color="white">
+                                            Time
+                                        </Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    <Tr>
+                                        <Td border="0px">0.32</Td>
+                                        <Td border="0px">$230.32</Td>
+                                        <Td border="0px">10:15:12</Td>
+                                    </Tr>
+                                    <Tr bg="rgba(98, 126, 234, 0.2)">
+                                        <Td border="0px">0.32</Td>
+                                        <Td border="0px">$230.32</Td>
+                                        <Td border="0px">10:15:12</Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td border="0px">0.32</Td>
+                                        <Td border="0px">$230.32</Td>
+                                        <Td border="0px">10:15:12</Td>
+                                    </Tr>
+                                    <Tr bg="rgba(98, 126, 234, 0.2)">
+                                        <Td border="0px">0.32</Td>
+                                        <Td border="0px">$230.32</Td>
+                                        <Td border="0px">10:15:12</Td>
+                                    </Tr>
+                                </Tbody>
+                            </Table>
+                        </Box>
+                    </HStack>
                     <PositionTab />
                 </Box>
                 <Box w="360px">
-                    <VStack spacing={6} p={0}>
+                    <VStack spacing={10} p={0}>
                         <TradeInput />
-                        {/* <Leverage /> */}
-                        <Summary />
+                        <Box background="#181B41" borderRadius="10px" p={6} w="100%">
+                            <Summary />
+                        </Box>
+                        <Box background="#181B41" borderRadius="10px" p={6} w="100%">
+                            <Slippage slippage={slippage} setSlippage={setSlippage} />
+                        </Box>
+                        <Button color="white" bgColor="#353E80" borderRadius="10px">
+                            Confirm Transaction
+                        </Button>
                     </VStack>
                 </Box>
             </Flex>
