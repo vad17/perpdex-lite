76e9b4829f20d2cb6ac3c217a4646d140c4cbf49
ttakuro <pochomukinsan@gmail.com>
2022-07-08 19:49:45 -0700

    created and refactored order history and table components

--- src/page/Trade/index.tsx
+++ src/page/Trade/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { VStack, Flex, Box, HStack, Text, Thead, Table, Tr, Th, Tbody, Td, Button, Divider } from "@chakra-ui/react"
+import { VStack, Flex, Box, HStack, Button, Divider } from "@chakra-ui/react"
 
 import ChartHead from "./ChartHead"
 import Summary from "./Summary"
@@ -8,6 +8,7 @@ import TradeInput from "./TradeInput"
 import PositionTab from "./PositionTab"
 import Slippage from "./Slippage"
 import LightWeightChart from "./LightWeightChart"
+import OrderHistory from "./OrderHistory"
 
 function Trade() {
     const [slippage, setSlippage] = useState<number>(0.5)
@@ -21,80 +22,7 @@ function Trade() {
                             <ChartHead />
                             <LightWeightChart />
                         </VStack>
-                        <Box
-                            w="100%"
-                            borderLeft="1px solid rgba(98, 126, 234, 0.6)"
-                            p={6}
-                            mx="0"
-                            marginInlineStart={[0, "0 !important"]}
-                            sx={{
-                                "@media screen and (max-width: 61em)": {
-                                    borderLeft: "0px none",
-                                    marginTop: "20px",
-                                },
-                            }}
-                        >
-                            <Text align="center" color={"gray.200"}>
-                                Order History
-                            </Text>
-                            <Table variant="simple" overflowY="scroll">
-                                <Thead>
-                                    <Tr>
-                                        <Th border="0px" color="white">
-                                            Size(ETH)
-                                        </Th>
-                                        <Th border="0px" color="white">
-                                            Price(USD)
-                                        </Th>
-                                        <Th border="0px" color="white">
-                                            Time
-                                        </Th>
-                                    </Tr>
-                                </Thead>
-                                <Tbody>
-                                    <Tr>
-                                        <Td border="0px">0.32</Td>
-                                        <Td border="0px">$230.32</Td>
-                                        <Td border="0px">10:15:12</Td>
-                                    </Tr>
-                                    <Tr bg="rgba(98, 126, 234, 0.2)">
-                                        <Td border="0px">0.32</Td>
-                                        <Td border="0px">$230.32</Td>
-                                        <Td border="0px">10:15:12</Td>
-                                    </Tr>
-                                    <Tr>
-                                        <Td border="0px">0.32</Td>
-                                        <Td border="0px">$230.32</Td>
-                                        <Td border="0px">10:15:12</Td>
-                                    </Tr>
-                                    <Tr bg="rgba(98, 126, 234, 0.2)">
-                                        <Td border="0px">0.32</Td>
-                                        <Td border="0px">$230.32</Td>
-                                        <Td border="0px">10:15:12</Td>
-                                    </Tr>
-                                    <Tr>
-                                        <Td border="0px">0.32</Td>
-                                        <Td border="0px">$230.32</Td>
-                                        <Td border="0px">10:15:12</Td>
-                                    </Tr>
-                                    <Tr bg="rgba(98, 126, 234, 0.2)">
-                                        <Td border="0px">0.32</Td>
-                                        <Td border="0px">$230.32</Td>
-                                        <Td border="0px">10:15:12</Td>
-                                    </Tr>
-                                    <Tr>
-                                        <Td border="0px">0.32</Td>
-                                        <Td border="0px">$230.32</Td>
-                                        <Td border="0px">10:15:12</Td>
-                                    </Tr>
-                                    <Tr bg="rgba(98, 126, 234, 0.2)">
-                                        <Td border="0px">0.32</Td>
-                                        <Td border="0px">$230.32</Td>
-                                        <Td border="0px">10:15:12</Td>
-                                    </Tr>
-                                </Tbody>
-                            </Table>
-                        </Box>
+                        <OrderHistory />
                     </Flex>
                     <Divider
                         borderColor="#627EEA"
