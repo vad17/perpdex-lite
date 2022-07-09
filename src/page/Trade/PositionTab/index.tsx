df832752b68eb6a4c90f45a82eea9f8e4efc8403
ttakuro <pochomukinsan@gmail.com>
2022-07-08 19:59:46 -0700

    refactored PositionTable comp

--- src/page/Trade/PositionTab/index.tsx
+++ src/page/Trade/PositionTab/index.tsx
@@ -13,11 +13,10 @@ import {
     Td,
     Divider,
 } from "@chakra-ui/react"
-import Button from "../../../component/base/Button"
 import { PerpdexExchangeContainer } from "container/connection/perpdexExchangeContainer"
 import { Modal } from "container/modal"
 import { useEffect, useMemo } from "react"
-import { PositionTableState } from "./PositionTable"
+import PositionTable, { PositionTableState } from "./PositionTable"
 import { PerpdexMarketContainer } from "../../../container/connection/perpdexMarketContainer"
 import { callSubquery } from "util/subquery"
 import { candles } from "queries/trades"
@@ -84,37 +83,7 @@ function PositionTab() {
 
             <TabPanels>
                 <TabPanel>
-                    <Table variant="simple">
-                        <Thead>
-                            <Tr>
-                                <StyledTh>Bitcoin - BTC</StyledTh>
-                                <StyledTh>Account Margin Ratio</StyledTh>
-                                <StyledTh>Amount (BTC)</StyledTh>
-                                <StyledTh>Liquidation Price</StyledTh>
-                                <StyledTh>Amount (USD)</StyledTh>
-                                <StyledTh>Leverage</StyledTh>
-                                <StyledTh>Avg. Open Price</StyledTh>
-                                <StyledTh>Profit/Loss</StyledTh>
-                                <StyledTh></StyledTh>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <StyledTd>Short</StyledTd>
-                                <StyledTd>45.3%</StyledTd>
-                                <StyledTd>0.23147</StyledTd>
-                                <StyledTd>$29787.78</StyledTd>
-                                <StyledTd>$4000</StyledTd>
-                                <StyledTd>4.2x</StyledTd>
-                                <StyledTd>$32680.78</StyledTd>
-                                <StyledTd>$350.27</StyledTd>
-                                <StyledTd>
-                                    <Button size="sm" customType="base-blue" text="Close Position"></Button>
-                                </StyledTd>
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    {/* {positionTableData && (
+                    {positionTableData && (
                         <PositionTable
                             marketState={positionTableData.marketState}
                             isLong={positionTableData.isLong}
@@ -126,7 +95,7 @@ function PositionTab() {
                             unrealizedPnl={positionTableData.unrealizedPnl}
                             handleOnClick={togglePositionCloseModal}
                         />
-                    )} */}
+                    )}
                 </TabPanel>
                 <TabPanel>
                     <Table variant="simple">
