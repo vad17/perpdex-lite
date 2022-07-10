2c9128daface8b82d32fdd5962f501884c42894c
ttakuro <pochomukinsan@gmail.com>
2022-07-10 00:32:03 -0700

    implemented deposit modal as Figma design

--- src/component/modals/AccountModal/index.tsx
+++ src/component/modals/AccountModal/index.tsx
@@ -8,10 +8,11 @@ import {
     Text,
     FormControl,
     Box,
+    FormLabel,
+    HStack,
 } from "@chakra-ui/react"
 import { AccountPerpdex } from "container/perpetual/account"
 import Button from "component/base/Button"
-import SmallFormLabel from "../../base/SmallFormLabel"
 import { formatInput, numberWithCommas } from "../../../util/format"
 import { INPUT_PRECISION } from "../../../constant"
 import { PerpdexMarketContainer } from "container/connection/perpdexMarketContainer"
@@ -71,20 +72,25 @@ function AccountModal() {
             isOpen={isAccountModalOpen}
             onClose={closeAccountModal}
             size="md"
+            p={3}
             body={
-                <Stack spacing={2}>
+                <Stack spacing={6} p={3}>
                     <FormControl id="margin">
-                        <SmallFormLabel>Amount</SmallFormLabel>
+                        <FormLabel>
+                            <Text fontSize="sm" color="white">
+                                Amount
+                            </Text>
+                        </FormLabel>
                         <NumberInput value={amount} onInput={handleOnInput}>
                             <InputGroup>
-                                <NumberInputField />
+                                <NumberInputField placeholder="1" />
                                 <InputRightElement w="54px">
                                     <Text
                                         w="100%"
                                         textAlign="center"
                                         fontWeight="bold"
                                         fontSize="xs"
-                                        color="blue.500"
+                                        color="white"
                                         textTransform="uppercase"
                                     >
                                         {currentMarketState?.quoteSymbol}
@@ -94,9 +100,19 @@ function AccountModal() {
                         </NumberInput>
                     </FormControl>
                     {isDeposit ? (
-                        <Box>{numberWithCommas(currentMyAccountInfo?.settlementTokenBalance)} available</Box>
+                        <HStack justifyContent="space-between">
+                            <Box color="rgba(255, 255, 255, 0.6)">
+                                {numberWithCommas(currentMyAccountInfo?.settlementTokenBalance)} available
+                            </Box>
+                            <Button size="xs" customType="base-blue" text="MAX" borderRadius="5px" />
+                        </HStack>
                     ) : (
-                        <Box>{numberWithCommas(currentMyAccountInfo?.collateralBalance)} available to withdraw</Box>
+                        <HStack justifyContent="space-between">
+                            <Box color="rgba(255, 255, 255, 0.6)">
+                                {numberWithCommas(currentMyAccountInfo?.collateralBalance)} available to withdraw
+                            </Box>
+                            <Button size="xs" customType="base-blue" text="MAX" borderRadius="5px" />
+                        </HStack>
                     )}
                 </Stack>
             }
@@ -106,6 +122,7 @@ function AccountModal() {
                     customType="base-blue"
                     onClick={handleSubmit}
                     isDisabled={!isEnabled}
+                    borderRadius="0px"
                 />
             }
         />
