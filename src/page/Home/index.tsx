6433e3fb0b8f55ac113cc233b9f78c5e19b3b81b
ttakuro <pochomukinsan@gmail.com>
2022-07-13 19:35:02 -0700

    removed unused components

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,16 +1,12 @@
-import { Center, Divider, Flex, VStack } from "@chakra-ui/react"
+import { Box, VStack } from "@chakra-ui/react"
 
 import FrameContainer from "component/FrameContainer"
 import AccountPanel from "../../component/AccountPanel"
 import AccountInfoTable from "component/AccountInfoTable"
-import TradeInfoPanel from "component/TradeInfoPanel"
-import OpenPositionsTable from "component/OpenPositionsTable"
 import { PerpdexMarketContainer } from "../../container/connection/perpdexMarketContainer"
 import { PerpdexExchangeContainer } from "../../container/connection/perpdexExchangeContainer"
 import { User } from "../../container/connection/user"
 import { AccountPerpdex } from "../../container/perpetual/account"
-import { getAllPositionsFromTakerInfos } from "util/position"
-import { useMemo } from "react"
 
 function Home() {
     const {
@@ -20,18 +16,11 @@ function Home() {
         actions: { openAccountModal },
     } = AccountPerpdex.useContainer()
     const { currentMyAccountInfo } = PerpdexExchangeContainer.useContainer()
-    const { currentMarketState, marketStates, setCurrentMarket } = PerpdexMarketContainer.useContainer()
-
-    const positions = useMemo(
-        () =>
-            currentMyAccountInfo?.takerInfos &&
-            getAllPositionsFromTakerInfos(currentMyAccountInfo.takerInfos, marketStates),
-        [currentMyAccountInfo?.takerInfos, marketStates],
-    )
+    const { currentMarketState } = PerpdexMarketContainer.useContainer()
 
     return (
         <FrameContainer>
-            <Flex direction={{ base: "column", lg: "row" }} w="100%" h="100%" justifyContent="space-evenly">
+            <Box>
                 <VStack w="100%">
                     <AccountPanel myAccountInfo={currentMyAccountInfo} marketState={currentMarketState} />
                     <AccountInfoTable
@@ -41,17 +30,7 @@ function Home() {
                         marketState={currentMarketState}
                     />
                 </VStack>
-                <Center h="100%" mx={10}>
-                    <Divider orientation="vertical" borderColor="#627EEA" />
-                </Center>
-                <VStack spacing={10} w="100%">
-                    <TradeInfoPanel myAccountInfo={currentMyAccountInfo} />
-                    <OpenPositionsTable
-                        data={positions}
-                        handleOnClick={(address: string) => setCurrentMarket(address)}
-                    />
-                </VStack>
-            </Flex>
+            </Box>
         </FrameContainer>
     )
 }
