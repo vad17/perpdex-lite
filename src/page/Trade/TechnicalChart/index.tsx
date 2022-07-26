7da8fdb189b239d70b00df5ea6f1eff4a8f009fd
Kei Shimizu <suns.manage.k@gmail.com>
2022-07-26 10:27:55 +0900

    added conditional module loading for technical chart

--- src/page/Trade/TechnicalChart/index.tsx
+++ src/page/Trade/TechnicalChart/index.tsx
@@ -1,16 +1,20 @@
 import React, { useMemo } from "react"
 import { PerpdexMarketContainer } from "container/connection/perpdexMarketContainer"
 import { createGetBars } from "util/chart"
-// import { getDatafeed } from "./datafeed"
-// import { ChartingLibraryWidgetOptions, IChartingLibraryWidget } from "./charting_library"
-// import { ResolutionString } from "./datafeed-api"
 import { Connection } from "container/connection"
 import { networkConfigs } from "constant/network"
-import PerpdexTradingView from "perpdex-tradingview"
+import { isTechnicalChart } from "constant/config"
+import * as PerpdexTradingView from "perpdex-tradingview"
+
+let perpdexTradingViewModule: typeof PerpdexTradingView
+
+if (isTechnicalChart) {
+    // eslint-disable-next-line unused-imports/no-unused-vars
+    perpdexTradingViewModule = require("perpdex-tradingview")
+}
 
 function TechnicalChart() {
     const { currentMarketState, marketStates } = PerpdexMarketContainer.useContainer()
-    // const [widget, setWidget] = useState<IChartingLibraryWidget | undefined>(undefined)
 
     const { chainId } = Connection.useContainer()
     const graphUri = networkConfigs[chainId || 81].thegraphEndpoint
@@ -25,32 +29,6 @@ function TechnicalChart() {
         isMarketReady,
         currentMarketState,
     ])
-
-    // useEffect(() => {
-    //     if (widget) return
-
-    //     if (graphUri && marketStates && Object.keys(marketStates).length > 0) {
-    //         const getBars = createGetBars(graphUri, marketStates)
-
-    //         const _widget = new window.TradingView.widget({
-    //             ...widgetOptions,
-    //             container: document.getElementById(widgetOptions.container),
-    //             datafeed: getDatafeed(getBars),
-    //         } as ChartingLibraryWidgetOptions)
-
-    //         _widget.onChartReady(() => {
-    //             console.log("Chart has loaded!")
-    //             setWidget(_widget)
-    //         })
-    //     }
-    // }, [graphUri, marketStates, widget])
-
-    // useEffect(() => {
-    //     if (widget) {
-    //         console.log("@@@@@@@ widget", widget)
-    //         widget.activeChart().setSymbol(currentMarketState.baseSymbol) // check symbol types
-    //     }
-    // }, [currentMarketState.baseSymbol, widget])
 
     const inputMarketState = useMemo(() => {
         if (isMarketReady) {
@@ -77,7 +55,8 @@ function TechnicalChart() {
         isMarketReady,
     ])
 
-    if (currentMarketState && inputMarketState && getBars) {
+    if (perpdexTradingViewModule && currentMarketState && inputMarketState && getBars) {
+        const PerpdexTradingView = perpdexTradingViewModule.default
         return <PerpdexTradingView marketState={inputMarketState} getBars={getBars} />
     }
 
