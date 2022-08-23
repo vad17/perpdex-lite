4d6cdb1f6dd9199051612333dc9e11cab1e4e3ea
ttakuro <pochomukinsan@gmail.com>
2022-08-23 01:08:26 -0700

    divided Trade Input componnent into two form components

---
+++ src/page/Trade/TradeInput/LimitForm.tsx
@@ -0,0 +1,138 @@
+import React, { useCallback, useEffect, useMemo, useState } from "react"
+import Big from "big.js"
+
+import { PerpdexMarketContainer } from "container/connection/perpdexMarketContainer"
+import PositionInput from "./PositionInput"
+import { BIG_ZERO } from "constant"
+import { PerpdexExchangeContainer } from "container/connection/perpdexExchangeContainer"
+import { Transaction } from "container/connection/transaction"
+import { VStack } from "@chakra-ui/react"
+import Button from "component/base/Button"
+import Summary from "./Summary"
+import _ from "lodash"
+import PriceInput from "./PriceInput"
+
+interface PreviewResult {
+    error: string
+    oppositeAmount: Big
+}
+
+interface LimitFormProps {
+    isBuyDisplay: boolean
+}
+
+function LimitForm({ isBuyDisplay }: LimitFormProps) {
+    const { currentMarketState } = PerpdexMarketContainer.useContainer()
+    const { preview, currentMyAccountInfo, createLimitOrder } = PerpdexExchangeContainer.useContainer()
+    const [limitPrice, setLimitPrice] = useState<Big>(BIG_ZERO)
+    const { isLoading } = Transaction.useContainer()
+    const [baseOrderValue, setBaseOrderValue] = useState<Big>(BIG_ZERO)
+    const [previewResult, setPreviewResult] = useState<PreviewResult>({
+        error: "",
+        oppositeAmount: Big(0),
+    })
+
+    const isBuy = useMemo(() => {
+        return currentMarketState?.inverse ? !isBuyDisplay : isBuyDisplay
+    }, [isBuyDisplay, currentMarketState?.inverse])
+
+    // TODO: apply correct values
+    const maxCollateral = useMemo(
+        () =>
+            currentMyAccountInfo && currentMyAccountInfo.collateralBalance
+                ? currentMyAccountInfo.collateralBalance.mul(10)
+                : BIG_ZERO,
+        [currentMyAccountInfo],
+    )
+
+    const handleBasePositionInput = useCallback((value: Big | null) => {
+        if (value !== null) {
+            setBaseOrderValue(value)
+        }
+    }, [])
+
+    const handlePriceInput = useCallback(
+        (value: Big | null) => {
+            if (value !== null) {
+                try {
+                    setLimitPrice(currentMarketState.inverse ? Big(1).div(value) : value)
+                } catch (err) {
+                    console.log(err)
+                }
+            }
+        },
+        [setLimitPrice, currentMarketState.inverse],
+    )
+
+    useEffect(() => {
+        if (currentMarketState.baseSymbol) {
+            setBaseOrderValue(BIG_ZERO)
+        }
+    }, [currentMarketState.baseSymbol])
+
+    const isSubmitDisabled = useMemo(() => {
+        return baseOrderValue.eq(0) || isLoading || !!previewResult.error
+    }, [baseOrderValue, isLoading, previewResult?.error])
+
+    const updatePreview = useCallback(async () => {
+        console.log("updatePreview")
+        const results = await preview.createLimitOrder(isBuy, baseOrderValue, limitPrice)
+        console.log(results)
+        if (!results) return
+        if (_.isString(results)) {
+            setPreviewResult({
+                error: results,
+                oppositeAmount: Big(0),
+            })
+        } else {
+            setPreviewResult({
+                error: "",
+                oppositeAmount: Big(0),
+            })
+        }
+    }, [preview, isBuy, baseOrderValue, limitPrice])
+
+    useEffect(() => {
+        updatePreview()
+    }, [updatePreview])
+
+    const handleSubmit = useCallback(async () => {
+        if (baseOrderValue) {
+            await createLimitOrder(isBuy, baseOrderValue, limitPrice)
+        }
+    }, [baseOrderValue, isBuy, createLimitOrder, limitPrice])
+
+    return (
+        <VStack spacing={6}>
+            <PositionInput
+                baseSymbol={currentMarketState.baseSymbol}
+                quoteSymbol={currentMarketState.quoteSymbol}
+                baseOrderValue={baseOrderValue}
+                markPrice={limitPrice}
+                maxCollateral={maxCollateral}
+                handleBasePositionInput={handleBasePositionInput}
+            />
+            <PriceInput
+                priceUnit={currentMarketState.priceUnitDisplay}
+                handlePriceInput={handlePriceInput}
+            ></PriceInput>
+            <Summary
+                isMarket={false}
+                error={previewResult.error}
+                baseAmount={baseOrderValue}
+                quoteAmount={previewResult.oppositeAmount}
+            />
+            <Button
+                w="100%"
+                text="Confirm Transaction"
+                onClick={handleSubmit}
+                isLoading={isLoading}
+                isDisabled={isSubmitDisabled}
+                customType="base-blue"
+                alignSelf="center"
+            />
+        </VStack>
+    )
+}
+
+export default LimitForm
--- src/page/Trade/TradeInput/index.tsx
+++ src/page/Trade/TradeInput/index.tsx
@@ -1,131 +1,12 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react"
-import Big from "big.js"
+import React, { useState } from "react"
 
-import { PerpdexMarketContainer } from "container/connection/perpdexMarketContainer"
-import PositionInput from "./PositionInput"
-import { BIG_ZERO } from "constant"
-import { PerpdexExchangeContainer } from "container/connection/perpdexExchangeContainer"
-import { Transaction } from "container/connection/transaction"
-import { Box, chakra, Tab, TabList, TabPanel, TabPanels, Tabs, VStack } from "@chakra-ui/react"
+import { Box, chakra, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react"
 import SideSwitcher from "component/base/SideSwitcher"
-import Button from "component/base/Button"
-import Summary from "./Summary"
-import Slippage from "./Slippage"
-import { bigNum2Big } from "util/format"
-import _ from "lodash"
-import PriceInput from "./PriceInput"
-
-interface PreviewResult {
-    error: string
-    oppositeAmount: Big
-}
+import MarketForm from "./MarketForm"
+import LimitForm from "./LimitForm"
 
 function TradeInput() {
-    const { currentMarketState } = PerpdexMarketContainer.useContainer()
-    const { trade, preview, currentMyAccountInfo, createLimitOrder } = PerpdexExchangeContainer.useContainer()
     const [isBuyDisplay, setIsBuyDisplay] = useState<boolean>(true)
-    const [isMarket, setIsMarket] = useState<boolean>(true)
-    const [limitPrice, setLimitPrice] = useState<Big>(BIG_ZERO)
-    const { isLoading } = Transaction.useContainer()
-    const [baseOrderValue, setBaseOrderValue] = useState<Big>(BIG_ZERO)
-    const [slippage, setSlippage] = useState<number>(2)
-    const [previewResult, setPreviewResult] = useState<PreviewResult>({
-        error: "",
-        oppositeAmount: Big(0),
-    })
-
-    const isBuy = useMemo(() => {
-        return currentMarketState?.inverse ? !isBuyDisplay : isBuyDisplay
-    }, [isBuyDisplay, currentMarketState?.inverse])
-
-    // TODO: apply correct values
-    const maxCollateral = useMemo(
-        () =>
-            currentMyAccountInfo && currentMyAccountInfo.collateralBalance
-                ? currentMyAccountInfo.collateralBalance.mul(10)
-                : BIG_ZERO,
-        [currentMyAccountInfo],
-    )
-
-    const handleBasePositionInput = useCallback((value: Big | null) => {
-        if (value !== null) {
-            setBaseOrderValue(value)
-        }
-    }, [])
-
-    const handlePriceInput = useCallback(
-        (value: Big | null) => {
-            if (value !== null) {
-                try {
-                    setLimitPrice(currentMarketState.inverse ? Big(1).div(value) : value)
-                } catch (err) {
-                    console.log(err)
-                }
-            }
-        },
-        [setLimitPrice, currentMarketState.inverse],
-    )
-
-    useEffect(() => {
-        if (currentMarketState.baseSymbol) {
-            setBaseOrderValue(BIG_ZERO)
-        }
-    }, [currentMarketState.baseSymbol])
-
-    const isSubmitDisabled = useMemo(() => {
-        return baseOrderValue.eq(0) || isLoading || !!previewResult.error
-    }, [baseOrderValue, isLoading, previewResult?.error])
-
-    const updatePreview = useCallback(async () => {
-        console.log("updatePreview")
-        if (isMarket) {
-            const results = await preview.trade(isBuy, baseOrderValue, slippage)
-            console.log(results)
-            if (!results) return
-            if (_.isString(results)) {
-                setPreviewResult({
-                    error: results,
-                    oppositeAmount: Big(0),
-                })
-            } else {
-                setPreviewResult({
-                    error: "",
-                    oppositeAmount: bigNum2Big(results),
-                })
-            }
-        } else {
-            const results = await preview.createLimitOrder(isBuy, baseOrderValue, limitPrice)
-            console.log(results)
-            if (!results) return
-            if (_.isString(results)) {
-                setPreviewResult({
-                    error: results,
-                    oppositeAmount: Big(0),
-                })
-            } else {
-                setPreviewResult({
-                    error: "",
-                    oppositeAmount: Big(0),
-                })
-            }
-        }
-    }, [isMarket, preview, isBuy, baseOrderValue, slippage, limitPrice])
-
-    useEffect(() => {
-        updatePreview()
-    }, [updatePreview])
-
-    const handleTradeSubmit = useCallback(async () => {
-        if (baseOrderValue) {
-            await trade(isBuy, baseOrderValue, slippage)
-        }
-    }, [baseOrderValue, trade, isBuy, slippage])
-
-    const handleLimitOrderSubmit = useCallback(async () => {
-        if (baseOrderValue) {
-            await createLimitOrder(isBuy, baseOrderValue, limitPrice)
-        }
-    }, [baseOrderValue, isBuy, createLimitOrder, limitPrice])
 
     const StyledTab = chakra(Tab, {
         baseStyle: {
@@ -151,61 +32,10 @@ function TradeInput() {
 
                     <TabPanels>
                         <TabPanel p={0}>
-                            <VStack spacing={6}>
-                                <PositionInput
-                                    baseSymbol={currentMarketState.baseSymbol}
-                                    quoteSymbol={currentMarketState.quoteSymbol}
-                                    baseOrderValue={baseOrderValue}
-                                    markPrice={currentMarketState.markPrice}
-                                    maxCollateral={maxCollateral}
-                                    handleBasePositionInput={handleBasePositionInput}
-                                />
-                                <Slippage slippage={slippage} setSlippage={setSlippage} />
-                                <Summary
-                                    isMarket={isMarket}
-                                    error={previewResult.error}
-                                    baseAmount={baseOrderValue}
-                                    quoteAmount={previewResult.oppositeAmount}
-                                />
-                                <Button
-                                    text="Confirm Transaction"
-                                    onClick={handleTradeSubmit}
-                                    isLoading={isLoading}
-                                    isDisabled={isSubmitDisabled}
-                                    customType="base-dark"
-                                    alignSelf="center"
-                                />
-                            </VStack>
+                            <MarketForm isBuyDisplay={isBuyDisplay} />
                         </TabPanel>
                         <TabPanel p={0}>
-                            <VStack spacing={6}>
-                                <PositionInput
-                                    baseSymbol={currentMarketState.baseSymbol}
-                                    quoteSymbol={currentMarketState.quoteSymbol}
-                                    baseOrderValue={baseOrderValue}
-                                    markPrice={limitPrice}
-                                    maxCollateral={maxCollateral}
-                                    handleBasePositionInput={handleBasePositionInput}
-                                />
-                                <PriceInput
-                                    priceUnit={currentMarketState.priceUnitDisplay}
-                                    handlePriceInput={handlePriceInput}
-                                ></PriceInput>
-                                <Summary
-                                    isMarket={isMarket}
-                                    error={previewResult.error}
-                                    baseAmount={baseOrderValue}
-                                    quoteAmount={previewResult.oppositeAmount}
-                                />
-                                <Button
-                                    text="Confirm Transaction"
-                                    onClick={handleLimitOrderSubmit}
-                                    isLoading={isLoading}
-                                    isDisabled={isSubmitDisabled}
-                                    customType="base-dark"
-                                    alignSelf="center"
-                                />
-                            </VStack>
+                            <LimitForm isBuyDisplay={isBuyDisplay} />
                         </TabPanel>
                     </TabPanels>
                 </Tabs>
