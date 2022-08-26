c5258c0cf3202240cdff45cb111828179329b766
a <a@example.com>
2022-08-26 15:51:57 +0900

    implement technical chart polling

--- src/tradingView/datafeed.ts
+++ src/tradingView/datafeed.ts
@@ -25,6 +25,62 @@ const configurationData = {
     supported_resolutions: ["1", "5", "60", "240", "1D"],
 }
 
+const resolutionToInterval = (resolution: string): number => {
+    return (
+        {
+            "1D": 24 * 60 * 60,
+        }[resolution] || +resolution * 60
+    )
+}
+
+interface RawBar {
+    low: number
+    high: number
+    close: number
+    volume: number
+}
+const barCache: { [address: string]: { [interval: string]: { [time: string]: RawBar } } } = {}
+
+const getBarCache = (address: string, interval: number) => {
+    barCache[address] = barCache[address] || {}
+    barCache[address][interval] = barCache[address][interval] || {}
+    return barCache[address][interval]
+}
+
+const addCandlesToCache = (address: string, interval: number, from: number, inverse: boolean, candles: any[]) => {
+    console.log("data feed addCandlesToCache", candles)
+    _.each(candles, (candle, idx: number) => {
+        if (candle.closeX96.eq(0)) return
+        getBarCache(address, interval)[from + idx * interval] = {
+            low: x96ToNumber(candle.lowX96, inverse),
+            high: x96ToNumber(candle.highX96, inverse),
+            close: x96ToNumber(candle.closeX96, inverse),
+            volume: bigNum2Big(candle.quote).toNumber(),
+        }
+    })
+}
+
+const getCacheBars = (address: string, interval: number, from: number, to: number) => {
+    let bars = _.map(getBarCache(address, interval), (bar, time) => {
+        return {
+            time: +time * 1000,
+            open: 0,
+            ...bar,
+        }
+    })
+    bars = _.sortBy(bars, "time")
+    _.each(bars, (bar, idx: number) => {
+        bar.open = bars[idx === 0 ? 0 : idx - 1].close
+    })
+    return _.filter(bars, bar => {
+        return from * 1000 <= bar.time && bar.time < to * 1000
+    })
+}
+
+const getCacheLastTime = (address: string, interval: number): number => {
+    return _.max(_.map(_.keys(getBarCache(address, interval)), _.toNumber)) || 0
+}
+
 export const createDataFeed = (config: DataFeedConfig) => {
     const { marketState } = config
     const contract = createMarketContract(config.marketState.address, config.signer)
@@ -68,6 +124,7 @@ export const createDataFeed = (config: DataFeedConfig) => {
                 supported_resolutions: configurationData.supported_resolutions,
                 volume_precision: 2,
                 data_status: "streaming",
+                has_empty_bars: true,
             }
 
             onSymbolResolvedCallback(symbolInfo)
@@ -82,44 +139,21 @@ export const createDataFeed = (config: DataFeedConfig) => {
             const { from, to, firstDataRequest } = periodParams
             console.log("data feed getBars called", symbolInfo, resolution, from, to)
 
-            const interval =
-                {
-                    "1D": 24 * 60 * 60,
-                }[resolution] || +resolution * 60
+            const interval = resolutionToInterval(resolution)
 
             try {
-                // TODO: chunk
-                let from2 = Math.max(0, Math.floor(from / interval) * interval)
-                const to2 = Math.max(0, Math.floor(to / interval) * interval)
-
-                let bars: any[] = []
+                const lastTime = getCacheLastTime(config.marketState.address, interval)
+                let from2 = Math.max(lastTime, Math.floor(from / interval) * interval)
+                const to2 = Math.floor(to / interval) * interval
+                console.log("data feed", lastTime, from2, to2)
                 while (from2 < to2) {
                     const step = Math.min(200, (to2 - from2) / interval)
                     const candles = await contract.getCandles(interval, from2, step)
-                    bars.push(
-                        _.map(candles, (candle, idx: number) => {
-                            return {
-                                time: (from2 + idx * interval) * 1000,
-                                low: x96ToNumber(candle.lowX96, inverse),
-                                high: x96ToNumber(candle.highX96, inverse),
-                                close: x96ToNumber(candle.closeX96, inverse),
-                                open: 0,
-                                volume: bigNum2Big(candle.quote).toNumber(),
-                            }
-                        }),
-                    )
+                    addCandlesToCache(config.marketState.address, interval, from2, inverse, candles)
                     from2 += step * interval
                 }
-                bars = _.flatten(bars)
-                console.log("data feed", bars)
 
-                bars = _.filter(bars, candle => {
-                    return candle.close !== 0
-                })
-                _.each(bars, (bar, idx: number) => {
-                    bar.open = bars[idx === 0 ? 0 : idx - 1].close
-                })
-
+                const bars = getCacheBars(config.marketState.address, interval, from, to)
                 console.log(`data feed getBars returned ${bars.length} bars`)
                 onHistoryCallback(bars, { noData: _.isEmpty(bars) })
             } catch (error) {
@@ -135,9 +169,15 @@ export const createDataFeed = (config: DataFeedConfig) => {
             onResetCacheNeededCallback: any,
         ) {
             console.log("data feed subscribeBars", symbolInfo, resolution, subscriberUID)
+            const interval = resolutionToInterval(resolution)
             const timerId = setInterval(async () => {
-                // TODO:
-            }, 60 * 1000)
+                const lastTime = getCacheLastTime(config.marketState.address, interval)
+                const candles = await contract.getCandles(interval, lastTime, 2)
+                addCandlesToCache(config.marketState.address, interval, lastTime, inverse, candles)
+                const bars = getCacheBars(config.marketState.address, interval, lastTime, lastTime + 2 * interval)
+                console.log("data feed subscription bars", bars)
+                _.each(bars, onRealtimeCallback)
+            }, 5 * 1000)
             subscriptions.push({
                 timerId: timerId,
                 subscriberUID: subscriberUID,
