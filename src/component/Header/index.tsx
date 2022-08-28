6ceed09561d18030d0d91890de913275fd25a921
ttakuro <pochomukinsan@gmail.com>
2022-08-28 02:01:17 -0700

    implemented hamburger menu and header for mobile

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,51 +1,106 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { Flex, Spacer, Button, HStack, Center, Heading } from "@chakra-ui/react"
+import {
+    Flex,
+    Spacer,
+    Button,
+    HStack,
+    Center,
+    Heading,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    IconButton,
+    useMediaQuery,
+} from "@chakra-ui/react"
 import { Logo } from "../Icon"
 import ConnectBtn from "./ConnectBtn"
 import NetworkBtn from "./NetworkBtn"
 import { positionTokenDisabled } from "../../constant"
+import { HamburgerIcon } from "@chakra-ui/icons"
 
 function Header() {
+    const isMobileAndTabletScreen = useMediaQuery("(max-width: 1024px)")
     return (
-        <Flex minWidth="max-content" h="90px" alignItems="center">
-            <Center marginLeft="24px">
-                <Link to="/">
-                    <Logo></Logo>
-                </Link>
-            </Center>
-            <Center marginLeft="18px" marginRight="18px">
-                <Link to="/">
-                    <Heading size="md">PerpDEX</Heading>
-                </Link>
-            </Center>
-            <Spacer />
-            <Center>
-                <HStack spacing={["24px", "30px", "42px", "80px"]}>
-                    <Link to="/">
-                        <Button variant="link">Home</Button>
-                    </Link>
-                    <Link to="/trade">
-                        <Button variant="link">Trade</Button>
-                    </Link>
-                    <Link to="/pools">
-                        <Button variant="link">Pool</Button>
-                    </Link>
-                    {!positionTokenDisabled && (
-                        <Link to="/tokens">
-                            <Button variant="link">Position Tokens</Button>
+        <>
+            {isMobileAndTabletScreen && isMobileAndTabletScreen[0] ? (
+                <Flex minWidth="max-content" h="90px" alignItems="center">
+                    <Menu isLazy>
+                        <MenuButton as={IconButton} icon={<HamburgerIcon boxSize={8} />} variant="unstyled" ml={4} />
+                        <MenuList>
+                            <MenuItem>
+                                <Link to="/">
+                                    <Logo></Logo>
+                                </Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to="/">
+                                    <Button variant="link">Home</Button>
+                                </Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to="/trade">
+                                    <Button variant="link">Trade</Button>
+                                </Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to="/pools">
+                                    <Button variant="link">Pool</Button>
+                                </Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to="/tokens">
+                                    <Button variant="link">Position Tokens</Button>
+                                </Link>
+                            </MenuItem>
+                        </MenuList>
+                    </Menu>
+                    <Spacer />
+                    <NetworkBtn />
+                    <ConnectBtn />
+                </Flex>
+            ) : (
+                <Flex minWidth="max-content" h="90px" alignItems="center">
+                    <Center marginLeft="24px">
+                        <Link to="/">
+                            <Logo></Logo>
                         </Link>
-                    )}
-                    {/*TODO: implement history*/}
-                    {/*<Link to="/history">*/}
-                    {/*    <Button variant="link">History</Button>*/}
-                    {/*</Link>*/}
-                </HStack>
-            </Center>
-            <Spacer />
-            <NetworkBtn />
-            <ConnectBtn />
-        </Flex>
+                    </Center>
+                    <Center marginLeft="18px" marginRight="18px">
+                        <Link to="/">
+                            <Heading size="md">PerpDEX</Heading>
+                        </Link>
+                    </Center>
+                    <Spacer />
+                    <Center>
+                        <HStack spacing={["24px", "30px", "42px", "80px"]}>
+                            <Link to="/">
+                                <Button variant="link">Home</Button>
+                            </Link>
+                            <Link to="/trade">
+                                <Button variant="link">Trade</Button>
+                            </Link>
+                            <Link to="/pools">
+                                <Button variant="link">Pool</Button>
+                            </Link>
+                            {!positionTokenDisabled && (
+                                <Link to="/tokens">
+                                    <Button variant="link">Position Tokens</Button>
+                                </Link>
+                            )}
+                            {/*TODO: implement history*/}
+                            {/*<Link to="/history">*/}
+                            {/*    <Button variant="link">History</Button>*/}
+                            {/*</Link>*/}
+                        </HStack>
+                    </Center>
+                    <Spacer />
+                    <NetworkBtn />
+                    <ConnectBtn />
+                </Flex>
+            )}
+        </>
     )
 }
 
